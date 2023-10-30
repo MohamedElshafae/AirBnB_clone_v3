8b048f5c5538e8328fd9b313fa70bc5c0decd0df
Elshafae <maohmedelshafae888@gmail.com>
2023-10-30 22:47:50 +0200

    change the names of functions

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -15,24 +15,24 @@ storage_type = getenv('HBNB_TYPE_STORAGE')
 
 @app_views.route('/places/<place_id>/amenities',
                  methods=['GET'], strict_slashes=False)
-def get_amenities(place_id):
+def get_place_amenities(place_id):
     """Get all Amenities."""
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
     if storage_type == 'db':
-        amenities = place.amenities
+        p_amenities = place.amenities
     else:
-        amenities = place.amenity_ids
+        p_amenities = place.amenity_ids
     all_amenities = []
-    for amenity in amenities:
+    for amenity in p_amenities:
         all_amenities.append(amenity.to_dict())
     return jsonify(all_amenities)
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['DELETE'], strict_slashes=False)
-def delete_amenity(place_id, amenity_id):
+def delete_place_amenity(place_id, amenity_id):
     """Delete amenity by id depending on storage."""
     amenity = storage.get(Amenity, amenity_id)
     place = storage.get(Place, place_id)
@@ -51,7 +51,7 @@ def delete_amenity(place_id, amenity_id):
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['POST'], strict_slashes=False)
-def create_amenity(place_id, amenity_id):
+def create_place_amenity(place_id, amenity_id):
     """Create amenities depending on storage."""
     amenity = storage.get(Amenity, amenity_id)
     place = storage.get(Place, place_id)
